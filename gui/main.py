44718a963a66db0ba29f939cc70c81d4c08a9e89
krawczyk071 <krawczyk071@gmail.com>
2023-08-26 21:19:36 +0200

    class based gui

--- gui/main.py
+++ gui/main.py
@@ -1,90 +1,151 @@
+from typing import Literal
+import os
+from PIL import ImageTk
+import PIL.Image
 import tkinter as tk
-
-# window = tk.Tk()
-# window.title("Temperature Converter")
-# window.resizable(width=False, height=False)
-
-# frame_a = tk.Frame()
-# label_a = tk.Label(master=frame_a, text="I'm in Frame A")
-# label_a.pack()
-
-# frame_b = tk.Frame(master=window, width=200, height=100, bg="red")
-# label_b = tk.Label(master=frame_b, text="I'm in Frame B")
-# label_b.pack()
-
-# # Swap the order of `frame_a` and `frame_b`
-# frame_b.pack(fill=tk.X)
-# frame_a.pack()
-
-# tk.Label(text="Hello, Tkinter").pack()
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename, askopenfilenames, askdirectory
 
 
-# def handle_click(event):
-#     print("The button was clicked!")
+class Gui(tk.Tk):
+    def __init__(self, title, width=300, height=200):
+        super().__init__()
+        self.title(title)
+        # self.resizable(width=False, height=False)
+        x = self.winfo_screenwidth() // 2
+        y = int(self.winfo_screenheight() // 2)
+        self.geometry(
+            f"{width}x{height}+" + str(x - (width // 2)) + "+" + str(y - (height // 2))
+        )
 
 
-# button = tk.Button(text="Click me!")
+class Frame(ttk.Frame):
+    def __init__(self, master):
+        super().__init__(master, width=100, height=100)
+        self.pack()
 
-# button.bind("<Button-1>", handle_click)
-# button.pack()
-
-
-# # def increase():
-# #     value = int(lbl_value["text"])
-# #     lbl_value["text"] = f"{value + 1}"
-
-
-# # def decrease():
-# #     value = int(lbl_value["text"])
-# #     lbl_value["text"] = f"{value - 1}"
+    def clear_widgets(self):
+        # select all frame widgets and delete them
+        for widget in self.winfo_children():
+            widget.destroy()
 
 
-# # btn_decrease = tk.Button(master=window, text="-", command=decrease)
-# # btn_decrease.grid(row=0, column=0, sticky="nsew")
-
-# # lbl_value = tk.Label(master=window, text="0")
-# # lbl_value.grid(row=0, column=1)
-
-# # btn_increase = tk.Button(master=window, text="+", command=increase)
-# # btn_increase.grid(row=0, column=2, sticky="nsew")
-
-# window.mainloop()
-
-# # .pack()
-# # .place()
-# # label2.place(x=75, y=75)
-# # .grid()
+class Label(ttk.Label):
+    def __init__(self, master, txt):
+        super().__init__(master, text=txt)
+        self.pack(),
 
 
-class Gui:
-    def __init__(self):
-        self.window = tk.Tk()
-        self.window.title("Temperature Converter")
-        self.window.resizable(width=False, height=False)
-        self.cbk = handle_click
-
-    def start(self):
-        self.window.mainloop()
-
-    def add_lbl(self, txt):
-        lbl = tk.Label(master=self.window, text=txt)
-        lbl.pack()
-
-    def add_btn(self, txt, cbk):
-        btn = tk.Button(text=txt)
-        btn.bind(func=self.cbk)
-        btn.pack()
+class TextOutput(tk.Text):
+    def __init__(self, master, txt, height=5, width=20):
+        super().__init__(master, height=height, width=width, padx=15, pady=15)
+        self.insert("1.0", txt)
+        self.pack()
 
 
-def handle_click(event):
-    # print(event)
-    print("The button was clicked!")
+class Image(ttk.Label):
+    def __init__(self, master, img_path, width=None, height=None, ratio=None):
+        img = self._prepare_img(img_path, width, height, ratio)
+        super().__init__(master, image=img)
+        self.image = img
+        self.pack()
+
+    def _prepare_img(self, img_path, width, height, ratio):
+        if bool(width) & bool(height):
+            i = PIL.Image.open(img_path)
+            i = i.resize((width, height))
+            # i.thumbnail((width,height), Image.ANTIALIAS)
+            return ImageTk.PhotoImage(i)
+        if ratio:
+            i = PIL.Image.open(img_path)
+            width, height = i.size
+            i = i.resize((int(width * ratio), int(height * ratio)))
+            return ImageTk.PhotoImage(i)
+        return ImageTk.PhotoImage(file=img_path)
 
 
-gui = Gui()
-gui.add_lbl("raz")
-gui.add_lbl("dwa")
+class Button(ttk.Button):
+    def __init__(self, master, txt, cbk):
+        super().__init__(master, text=txt, command=cbk)
+        self.pack()
 
-gui.add_btn("raz", handle_click)
-gui.add_btn("dwa", handle_click)
-gui.start()
+
+class Input(ttk.Entry):
+    def __init__(self, master):
+        txt_var = tk.StringVar()
+        super().__init__(master, textvariable=txt_var)
+
+        self.pack(pady=20)
+
+
+class FileDialog(ttk.Button):
+    def __init__(
+        self,
+        master,
+        txt,
+        mode: Literal["folder", "file", "files"],
+        title=None,
+        dir_init=None,
+        filetypes=None,
+    ):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
+        self.path = None
+        self.mode = mode
+        self.title = title
+        self.dir_init = dir_init if dir_init else self.dir_path
+        self.filetypes = (
+            filetypes if filetypes else [("text files", "*.txt"), ("All files", "*.*")]
+        )
+        super().__init__(master, text=txt, command=lambda: self._save_path())
+        self.pack()
+        # self.bind("<Button-1>", lambda event: self._save_path())
+
+    def get(self):
+        return self.path
+
+    def _save_path(self):
+        self.path = self._select_func()
+        print(self.path)
+
+    def _select_func(self):
+        match self.mode:
+            case "folder":
+                return askdirectory(
+                    initialdir=self.dir_init,
+                    title=self.title if self.title else "Select directory",
+                )
+            case "file":
+                return askopenfilename(
+                    initialdir=self.dir_init,
+                    title=self.title if self.title else "Select file",
+                    filetypes=self.filetypes,
+                )
+            case "files":
+                return askopenfilenames(
+                    initialdir=self.dir_init,
+                    title=self.title if self.title else "Select files",
+                    filetypes=self.filetypes,
+                )
+            # filedialog.asksaveasfilename()
+            # filedialog.asksaveasfile()
+            # filedialog.askopenfile()
+            # filedialog.askopenfiles()
+
+
+def testprint(x):
+    print(x)
+
+
+gui = Gui("tytul", 300, 400)
+frm1 = Frame(gui)
+lbl1 = Label(frm1, "test1")
+btn1 = Button(frm1, "test1x", testprint)
+btn2 = Button(frm1, "test1y", lambda: testprint("yy"))
+img1 = Image(frm1, "wykres1.png", width=100, height=100)
+ipt1 = Input(frm1)
+btn1 = Button(frm1, "print ipt", lambda: print(ipt1.get()))
+txt1 = TextOutput(frm1, "test1")
+fd1 = FileDialog(frm1, "open", "files")
+btn2 = Button(frm1, "destroy", lambda: frm1.clear_widgets())
+
+gui.mainloop()
